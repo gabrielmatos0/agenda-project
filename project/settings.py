01845e7cfe3c7b196259e4906a3353b9829e3df4
Spyware <gabrielluisdm13@gmail.com>
2025-11-30 18:54:20 -0300

    Configuring media path to store pictures

--- project/settings.py
+++ project/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR , 'base_statics')]
+STATIC_ROOT = BASE_DIR / 'static' # cllectstatic
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
