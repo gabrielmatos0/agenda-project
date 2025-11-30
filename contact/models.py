237c57c44446ef6355180e3b39a34d141514cccc
Spyware <gabrielluisdm13@gmail.com>
2025-11-30 18:54:48 -0300

    Models

--- contact/models.py
+++ contact/models.py
@@ -6,8 +6,14 @@ from django.utils import timezone
 # email (email), created_date (date), description (text)
 
 # Depois
-# category (foreign key), show (boolean), owner (foreign key)
-# piture (imagem)
+# category (foreign key), show (boolean), piture (imagem)
+# owner (foreign key)
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=25)
@@ -16,6 +22,15 @@ class Contact(models.Model):
     email = models.EmailField(max_length=100, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to="pictures/%Y/%m")
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+
 
     def __str__(self):
         return self.first_name + ' | ' + self.phone
