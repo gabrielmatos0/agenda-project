e35b2f8d5fcb2d33d8cf586955ebf6377749f456
Spyware <gabrielluisdm13@gmail.com>
2025-11-30 18:53:01 -0300

    Admin display

--- contact/admin.py
+++ contact/admin.py
@@ -3,4 +3,15 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    pass
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'email',
+    ordering = '-id', 'first_name', 'phone', 'last_name',
+    # list_filter = 'created_date', 'phone',
+    search_fields = 'first_name', 'last_name', 'phone', 'email',
+    list_per_page = 1
+    list_max_show_all = 5
+    list_editable = 'first_name', 'last_name',
+    # list_display_links = 'first_name',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ordering = '-id',
