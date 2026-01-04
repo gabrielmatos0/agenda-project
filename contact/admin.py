1b3454f66ae498f702caad11bb85561e9c21dc21
Spyware <gabrielluisdm13@gmail.com>
2026-01-04 17:23:01 -0300

    Contact admin

--- contact/admin.py
+++ contact/admin.py
@@ -7,8 +7,8 @@ class ContactAdmin(admin.ModelAdmin):
     ordering = '-id', 'first_name', 'phone', 'last_name',
     # list_filter = 'created_date', 'phone',
     search_fields = 'first_name', 'last_name', 'phone', 'email',
-    list_per_page = 1
-    list_max_show_all = 5
+    list_per_page = 10
+    list_max_show_all = 20
     list_editable = 'first_name', 'last_name',
     # list_display_links = 'first_name',
 
