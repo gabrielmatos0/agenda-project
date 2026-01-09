5f4cca2e85b70da950e92f8969244b740e8ca3a7
gabrielmatos0 <gabrielluisdm13@gmail.com>
2026-01-08 21:00:17 -0300

    displaying contacts

--- contact/admin.py
+++ contact/admin.py
@@ -3,13 +3,13 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone', 'email',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'email', 'show',
     ordering = '-id', 'first_name', 'phone', 'last_name',
     # list_filter = 'created_date', 'phone',
     search_fields = 'first_name', 'last_name', 'phone', 'email',
     list_per_page = 10
     list_max_show_all = 20
-    list_editable = 'first_name', 'last_name',
+    list_editable = 'first_name', 'last_name', 'show',
     # list_display_links = 'first_name',
 
 @admin.register(models.Category)
--- contact/models.py
+++ contact/models.py
@@ -22,7 +22,7 @@ class Category(models.Model):
 class Contact(models.Model):
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25, blank=True)
-    phone = models.CharField(max_length=15)
+    phone = models.CharField(max_length=30)
     email = models.EmailField(max_length=100, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
