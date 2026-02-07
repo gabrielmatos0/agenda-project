82f4a5a02e846810358b40c9a12a17f4977b409e
gabrielmatos0 <gabrielluisdm13@gmail.com>
2026-02-07 18:31:37 -0300

    Add form action

--- contact/urls.py
+++ contact/urls.py
@@ -5,7 +5,11 @@ app_name = 'contact'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('contact/<int:contact_id>/', views.contact, name='contact'),
-    path('contact/create/', views.create, name='create'),
     path('contact/search/', views.search, name='search'),
+
+    # contact (CRUD)
+    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
 ]
