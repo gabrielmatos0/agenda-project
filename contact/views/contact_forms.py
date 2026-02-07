01d73036463ef43d88cdc8e044e2c4ee4b7d0af4
gabrielmatos0 <gabrielluisdm13@gmail.com>
2026-02-07 19:08:52 -0300

    Add contact delete function

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -51,3 +51,19 @@ def update(request, contact_id):
         'form_action': form_action,
     }
     return render(request, 'contact/create.html', context)
+
+
+def delete(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+
+    context = {
+        'contact': contact,
+        'confirmation': confirmation
+    }
+
+    return render(request, 'contact/contact.html', context)
